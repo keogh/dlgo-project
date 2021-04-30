ee845aaa39585c21d277e1704a0c79d1c620529d
Isaac Zepeda <keogh24@gmail.com>
2021-04-30 17:31:25 -0600

    Move modules

--- bot_v_bot.py
+++ bot_v_bot.py
@@ -1,12 +1,12 @@
 from dlgo import agent
-from dlgo import goboard
+from dlgo import goboard_slow
 from dlgo import gotypes
 from dlgo.utils import print_board, print_move
 import time
 
 def main():
     board_size = 9
-    game = goboard.GameState.new_game(board_size)
+    game = goboard_slow.GameState.new_game(board_size)
     bots = {
         gotypes.Player.black: agent.naive.RandomBot(),
         gotypes.Player.white: agent.naive.RandomBot(),
